42bb38d94d7d8ce0fef6475312ad29c9f89448e3
waisalizai <wais.ahmad.ahmadi@gmail.com>
2026-06-18 19:12:39 +0100

    console.log(bmi) added in line 21

--- script.js
+++ script.js
@@ -16,7 +16,9 @@ function calculateBmi () {
     let weight = document.getElementById ('weight').value
     let height = document.getElementById ('height').value
 
-    
+    let bmi = (weight/(height * height) * 70)
 
+    console.log(bmi)
+    
 
 }
