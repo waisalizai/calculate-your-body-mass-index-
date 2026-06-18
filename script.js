e9704bddaad2128dbada9877db7267183e31d1f7
waisalizai <wais.ahmad.ahmadi@gmail.com>
2026-06-18 19:34:59 +0100

    more js codes written

--- script.js
+++ script.js
@@ -12,7 +12,6 @@
 
 function calculateBmi () {
 
-
     let weight = document.getElementById ('weight').value
     let height = document.getElementById ('height').value
 
@@ -21,5 +20,19 @@ function calculateBmi () {
     document.getElementById ("heading").innerHTML = 'Your BMI is :'
     document.getElementById ('bmi-output').innerHTML = bmi
 
+    if (bmi <= 24.9) {
+        document.getElementById ('message').innerHTML = 'You are underweight'
+    } else if (bmi >= 25 && bmi <= 29.9)
+
+        {
+             document.getElementById ('message').innerHTML = 'You are a healthy weight'
+        } else {
+             document.getElementById ('message').innerHTML = 'You are overweight'
+        }
+         
+
+
 
 }
+
+
