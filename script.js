4ee068bc8725a263692cb15a224e2a8e9f64f2f6
waisalizai <wais.ahmad.ahmadi@gmail.com>
2026-06-23 17:41:11 +0100

    changed to kg and cm

--- script.js
+++ script.js
@@ -1,39 +1,28 @@
 
 
 
-// function 
-// get values 
-// calculate 
-// output 
+function calculateBmi() {
 
-// bmi formula weight / height squard * 703
+    let weight = parseFloat(document.getElementById('weight').value);
+    let height = parseFloat(document.getElementById('height').value);
 
+    let bmi = weight / ((height / 100) * (height / 100));
 
+    document.getElementById("heading").innerHTML = 'Your BMI is:';
+    document.getElementById('bmi-output').innerHTML = bmi.toFixed(1);
 
-function calculateBmi () {
-
-    let weight = document.getElementById ('weight').value
-    let height = document.getElementById ('height').value
-
-    let bmi = (weight/(height * height) * 703)
-
-    document.getElementById ("heading").innerHTML = 'Your BMI is :'
-    document.getElementById ('bmi-output').innerHTML = bmi.toFixed (1)
-
-    if (bmi <= 24.9) {
-        document.getElementById ('message').innerHTML = 'You are underweight'
-    } else if (bmi >= 25 && bmi <= 29.9)
-
-        {
-             document.getElementById ('message').innerHTML = 'You are a healthy weight'
-        } else {
-             document.getElementById ('message').innerHTML = 'You are overweight'
-        }
-
+    if (bmi < 18.5) {
+        document.getElementById('message').innerHTML = 'You are underweight';
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        document.getElementById('message').innerHTML = 'You are a healthy weight';
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        document.getElementById('message').innerHTML = 'You are overweight';
+    } else {
+        document.getElementById('message').innerHTML = 'You are obese';
+    }
 }
 
-function reload () {
-window.location.reload()
-
+function reload() {
+    window.location.reload();
 }
 
